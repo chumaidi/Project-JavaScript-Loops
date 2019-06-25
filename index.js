6de8aa0f1c74b4c31039e3d73d44e12c9699d278
chumaidi <chumaidihxc@gmail.com>
2019-06-25 15:15:27 +0700

    plus commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let iteration = 1
+
+do {
+  console.log(iteration)
+  iteration += 5
+} while (iteration < 100
+
+
+for (let index = 0; index <= 100; index += 10) {
+    console.log(index)
+  }
+  
+
+let iteration = 1
+
+do {
+  console.log(iteration)
+  iteration += 5
+} while (iteration < 100)
+
+for (let index = 0; index <= 100; index += 10) {
+    console.log(index)
+  }
+
+  const numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index]
+  console.log(element)
+}
