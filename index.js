a6c9ac14f22e278cddea886a50e02e0a85f0b31e
chumaidi <43468985+chumaidi@users.noreply.github.com>
2019-06-26 09:54:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ let iteration = 1
 do {
   console.log(iteration)
   iteration += 5
-} while (iteration < 100
-
+while (iteration < 100
+} 
 
 for (let index = 0; index <= 100; index += 10) {
     console.log(index)
   }
   
 
-let iteration = 1
-
 do {
   console.log(iteration)
   iteration += 5
-} while (iteration < 100)
+ while (iteration < 100)
+}
 
 for (let index = 0; index <= 100; index += 10) {
     console.log(index)
